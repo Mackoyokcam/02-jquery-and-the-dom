706d08f93c1d139f7cc04d8ccf775d309c76eada
mackoyokcam <mark.hangarter@gmail.com>
2017-08-15 14:33:19 -0700

    figured out how to add article data to html. Done!

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -17,21 +17,12 @@ Article.prototype.toHtml = function() {
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest
-  of the current template clone with properties from this particular Article instance.
-  We need to fill in:
 
-    1. author name,
-    2. author url,
-    3. article title,
-    4. article body, and
-    5. publication date. */
-  $('category h1').text(this.title);
-  $('category a').text(this.author);
-  $('category a').href(this.authorUrl);
-  $('category time').text(this.publishedOn);
-  $('category section').text(this.body);
-
+  $newArticle.find('h1').text(this.title);
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
+  $newArticle.find('time').text(this.publishedOn);
+  $newArticle.find('section').text(this.body);
 
   // Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
