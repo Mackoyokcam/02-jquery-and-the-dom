0d35e29c4acfedd26fa4950b3d8ee2a26f790971
mackoyokcam <mark.hangarter@gmail.com>
2017-08-15 13:48:41 -0700

    added script tags, and jquery, finished article constructor

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,10 +1,13 @@
 'use strict';
-
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
-  // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
@@ -43,5 +46,6 @@ rawData.forEach(function(articleObject) {
 });
 
 articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
+  // $('#articles').append(article.toHtml());
+  $('#articles').append('<p>Hello</p>');
 });
